1f67970bbf90be1854883fa8996863922cbc40fb
DavidAguayo <AlumnoMCSD@T06W02>
2018-03-13 12:46:52 +0100

    primer commit

---
+++ PruebaGitHub/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaGitHub
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
